f217409efa66e8fffd733f2c26ce7082e89e3d7d
SoftwareMaestro16 <softwaremaestro16@gmail.com>
2025-03-20 12:32:40 +0200

    add

--- src/main.tsx
+++ src/main.tsx
@@ -23,14 +23,14 @@ createRoot(document.getElementById('root')!).render(
           },
           [THEME.LIGHT]: { 
             connectButton: {
-              background: '#FFD700', 
+              background: '#ffaa33', 
             },
             accent: '#FFD700', 
             telegramButton: '#FFD700', 
             background: {
-              primary: '#FFD700',
-              secondary: '#FFD700', 
-              tint: '#FFD700'
+              primary: '#ffaa33',
+              secondary: '#ffaa33', 
+              tint: '#2b2b2b'
             }
           }
         },
